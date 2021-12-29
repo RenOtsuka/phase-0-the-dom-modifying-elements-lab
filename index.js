8dfaebe48a1b2317915771b83b7ed3aacec2ae91
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2021-12-29 16:05:14 -0600

    Completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main1 = document.querySelector('main');
+main1.remove();
+
+const newHeader = document.createElement('h1');
+document.body.append(newHeader);
+newHeader.nodeName = 'H1';
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "Ryan is the champion";
